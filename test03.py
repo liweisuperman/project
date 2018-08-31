6ab6cf653e12ae5efa327b6870c8b1f6fbea725b
liweisuperman <42856749+liweisuperman@users.noreply.github.com>
2018-08-31 16:32:44 +0800

    Update test03.py

--- test03.py
+++ test03.py
@@ -1,5 +1,6 @@
 def func1():
     for i in range(1,10):
         for j in range(1,i+1):
-	    print(i+"x"+j+"="+i*j,end=" ") 		      print()
+	    print(i+"x"+j+"="+i*j,end=" ") 		     
+	print()
 func1()
